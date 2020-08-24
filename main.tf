0f90d9b5704a5640662df97a3b8e04ccbb46a2e5
kira <kiradell777@gmail.com>
2020-08-24 14:11:43 +0200

    bucket names fixed

--- main.tf
+++ main.tf
@@ -2,5 +2,5 @@
 data "aws_caller_identity" "myAccount" {}
 resource "aws_s3_bucket" "myBucket" {
   count = var.numberOfBuckets 
-  bucket = "my_terraform_bucket_${var.bucketName}"
+  bucket = "my-terraform-bucket-${var.bucketName}"
 }
