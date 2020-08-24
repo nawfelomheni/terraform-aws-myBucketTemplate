f793bf85fb9af75fa034c3d57e00f97ee379c67a
kira <kiradell777@gmail.com>
2020-08-24 11:47:39 +0200

    nested provider configuration issue is fixed

--- main.tf
+++ main.tf
@@ -6,5 +6,5 @@ provider "aws" {
 data "aws_caller_identity" "myAccount" {}
 resource "aws_s3_bucket" "myBucket" {
   count = var.numberOfBuckets 
-  bucket = "my_terraform_bucket_${data.aws_caller_identity.myAccount.account_id}_${var.bucketName}"
+  bucket = "my_terraform_bucket_${var.bucketName}"
 }
